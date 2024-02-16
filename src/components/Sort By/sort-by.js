3bfdda3b6550e72424b803be676eb4a089208137
suyash002 <getontosuyash@gmail.com>
2024-02-16 10:48:13 +0530

    filter completed

--- src/components/Sort By/sort-by.js
+++ src/components/Sort By/sort-by.js
@@ -1,25 +1,89 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 
-function Sortby() {
-    return (
-        <div className='sort-by'>
-            
-                <h6>Sort By:</h6>
-               
-                    <select className="form-control">
-                        {/* <option value="default">Sort By:</option> */}
-                        <option value="A-Z">Alphabetically, A-Z</option>
-                        <option value="Z-A">Alphabetically, Z-A</option>
-                        <option value="PriceLowToHigh">Price, Low to High</option>
-                        <option value="PriceHighToLow">Price, High to Low</option>
-                        <option value="DateOldToNew">Date, Old to New</option>
-                        <option value="DateNewToOld">Date, New to Old</option>
-                    </select>
-                
+function Sortby({ data, setData }) {
+  const [selectedOption, setSelectedOption] = useState("");
 
-        </div>
-    )
+  const handleSortChange = (e) => {
+    const value = e.target.value;
+    setSelectedOption(value);
+
+    // Ensure data is an array before sorting
+    if (Array.isArray(data)) {
+      let sortedData = [...data];
+
+      switch (value) {
+        case "A-Z":
+          sortedData.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case "Z-A":
+          sortedData.sort((a, b) => b.name.localeCompare(a.name));
+          break;
+        case "PriceLowToHigh":
+          sortedData.sort((a, b) => a.price - b.price);
+          break;
+        case "PriceHighToLow":
+          sortedData.sort((a, b) => b.price - a.price);
+          break;
+        case "DateOldToNew":
+          sortedData.sort((a, b) => new Date(a.date) - new Date(b.date));
+          break;
+        case "DateNewToOld":
+          sortedData.sort((a, b) => new Date(b.date) - new Date(a.date));
+          break;
+        default:
+          break;
+      }
+
+      setData(sortedData);
+    } else {
+      console.error("Data is not an array.");
+    }
+  };
+
+  return (
+    <div className="sort-by">
+      <h6>Sort By:</h6>
+      <select
+        className="form-control"
+        // value={selectedOption}
+        // onChange={handleSortChange}
+      >
+        <option value="">Sort By:</option>
+        <option value="A-Z">Alphabetically, A-Z</option>
+        <option value="Z-A">Alphabetically, Z-A</option>
+        <option value="PriceLowToHigh">Price, Low to High</option>
+        <option value="PriceHighToLow">Price, High to Low</option>
+        <option value="DateOldToNew">Date, Old to New</option>
+        <option value="DateNewToOld">Date, New to Old</option>
+      </select>
+    </div>
+  );
 }
 
-export default Sortby
+export default Sortby;
+
+// import React from 'react'
+// import { Col, Row } from 'react-bootstrap'
+
+// function Sortby() {
+//     return (
+//         <div className='sort-by'>
+
+//                 <h6>Sort By:</h6>
+
+//                     <select className="form-control">
+//                         {/* <option value="default">Sort By:</option> */}
+//                         <option value="A-Z">Alphabetically, A-Z</option>
+//                         <option value="Z-A">Alphabetically, Z-A</option>
+//                         <option value="PriceLowToHigh">Price, Low to High</option>
+//                         <option value="PriceHighToLow">Price, High to Low</option>
+//                         <option value="DateOldToNew">Date, Old to New</option>
+//                         <option value="DateNewToOld">Date, New to Old</option>
+//                     </select>
+
+//         </div>
+//     )
+// }
+
+// export default Sortby
--- src/components/language/multi-language.js
+++ src/components/language/multi-language.js
@@ -58,24 +58,24 @@ const Multilang = () => {
   //     }
   //   };
   // }, []);
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-    script.async = true;
-    document.body.appendChild(script);
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src =
+  //     "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+  //   script.async = true;
+  //   document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
-  const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      { pageLanguage: "en" },
-      "google_translate_element"
-    );
-  };
+  // const googleTranslateElementInit = () => {
+  //   new window.google.translate.TranslateElement(
+  //     { pageLanguage: "en" },
+  //     "google_translate_element"
+  //   );
+  // };
 
   return (
     <div className="mylang">
